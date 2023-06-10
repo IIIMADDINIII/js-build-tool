de237f98cd86b295603341cb3c2282e72fc60dd6
IIIMADDINIII <martin@elend.name>
2023-06-10 21:39:30 +0200

    testing bundeling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-// Reference Switches so that the const:production and const:development packages are defined
-/// <reference types="rollup-config-iiimaddiniii/switches" />
-import * as path from "path";
-import * as url from 'url';
-//const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+import * as gulpLocal from "gulp";
 
-let cwd = process.cwd();
-let packageDir = path.resolve(__dirname, "..");
-
-console.log(cwd, packageDir, process.argv);
+export const gulp = gulpLocal;
