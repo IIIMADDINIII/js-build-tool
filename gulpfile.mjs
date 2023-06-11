b55d71fe3a1271c3eae259eaedec067a5626483a
IIIMADDINIII <martin@elend.name>
2023-06-11 03:37:32 +0200

    fix gulpfile errors

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,25 +1,25 @@
 import { exec } from "@iiimaddiniii/js-build-tool/execa";
 import { rimraf } from "@iiimaddiniii/js-build-tool/rimraf";
 import { series, parallel } from "@iiimaddiniii/js-build-tool/gulp";
-import { prodSelectPnpmAndInstall, selectPnpmAndInstall, cleanWithGit } from "@iiimaddiniii/js-build-tool";
+import { file, cleanWithGit, selectPnpmAndInstall, prodSelectPnpmAndInstall, isProd } from "@iiimaddiniii/js-build-tool";
 import * as fs from "fs/promises";
 import * as path from "path";
 
 async function bundle() {
   await exec("pnpm install");
   let env = {};
-  if (prod) {
+  if (isProd) {
     env.prod = "true";
   }
   await exec("pnpm exec rollup --config node:iiimaddiniii", { env });
 }
 
 async function packageModules() {
-  let tmpBuildDir = path.resolve(cwd, ".tmpBuild");
-  let src = path.resolve(cwd, "packageDependencies.json");
-  let dest = path.resolve(tmpBuildDir, "package.json");
-  let srcDir = path.resolve(tmpBuildDir, "node_modules");
-  let destDir = path.resolve(cwd, "modules");
+  let tmpBuildDir = file(".tmpBuild");
+  let src = file("packageDependencies.json");
+  let dest = file(".tmpBuild/package.json");
+  let srcDir = file(".tmpBuild/node_modules");
+  let destDir = file("modules");
   await rimraf([tmpBuildDir, destDir]);
   await fs.mkdir(tmpBuildDir);
   await fs.copyFile(src, dest);
@@ -27,6 +27,6 @@ async function packageModules() {
   await fs.rename(srcDir, destDir);
 }
 
-export const clean = series(selectPnpmAndInstall, cleanWithGit);
-export const build = series(selectPnpmAndInstall, parallel(bundle, packageModules));
-export const buildCi = series(prodSelectPnpmAndInstall, cleanWithGit, parallel(bundle, packageModules));
+export const clean = series(selectPnpmAndInstall(), cleanWithGit);
+export const build = series(selectPnpmAndInstall(), parallel(bundle, packageModules));
+export const buildCi = series(prodSelectPnpmAndInstall(), cleanWithGit, parallel(bundle, packageModules));
