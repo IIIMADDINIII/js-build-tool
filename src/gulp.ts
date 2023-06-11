18d1d034a16c8732f520b30272a16df86d7cfe21
IIIMADDINIII <martin@elend.name>
2023-06-11 02:44:17 +0200

    Fixed named exports

--- src/gulp.ts
+++ src/gulp.ts
@@ -1,5 +1,13 @@
 import * as gulp from "gulp";
-import { dest, lastRun, parallel, registry, series, src, symlink, task, tree, watch } from "gulp";
 
-export { src, dest, symlink, watch, task, series, parallel, registry, tree, lastRun };
 export default gulp;
+export const src: typeof gulp.src = gulp.src;
+export const dest: typeof gulp.dest = gulp.dest;
+export const symlink: typeof gulp.symlink = gulp.symlink;
+export const watch: typeof gulp.watch = gulp.watch;
+export const task: typeof gulp.task = gulp.task;
+export const series: typeof gulp.series = gulp.series;
+export const parallel: typeof gulp.parallel = gulp.parallel;
+export const registry: typeof gulp.registry = gulp.registry;
+export const tree: typeof gulp.tree = gulp.tree;
+export const lastRun: typeof gulp.lastRun = gulp.lastRun;
