35f435e40b80689ecaaf370b3060977e821ee38b
IIIMADDINIII <martin@elend.name>
2023-06-15 20:43:59 +0200

    Updating to new version

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,14 +1,14 @@
 import * as fs from "fs/promises";
 import { exec } from "gulp-execa";
 import { rimraf } from "rimraf";
-import { file, tasks, rollup, series, parallel, readJson } from "@iiimaddiniii/js-build-tool";
+import { tools, tasks, rollup } from "@iiimaddiniii/js-build-tool";
 
 async function packageModules() {
-  let tmpBuildDir = file(".tmpBuild");
-  let src = file("packageDependencies.json");
-  let dest = file(".tmpBuild/package.json");
-  let srcDir = file(".tmpBuild/node_modules");
-  let destDir = file("modules");
+  let tmpBuildDir = tools.file(".tmpBuild");
+  let src = tools.file("packageDependencies.json");
+  let dest = tools.file(".tmpBuild/package.json");
+  let srcDir = tools.file(".tmpBuild/node_modules");
+  let destDir = tools.file("modules");
   await rimraf([tmpBuildDir, destDir]);
   await fs.mkdir(tmpBuildDir);
   await fs.copyFile(src, dest);
@@ -16,9 +16,9 @@ async function packageModules() {
   await fs.rename(srcDir, destDir);
 }
 
-let deps = Object.keys((await readJson("packageDependencies.json")).dependencies);
+let deps = Object.keys((await tools.readJson("packageDependencies.json")).dependencies);
 const bundle = rollup.tasks.build({ blacklistDevDependencies: false, externalDependencies: deps }, { failAfterWarnings: false });
 
-export const clean = series(tasks.cleanWithGit);
-export const build = series(tasks.selectPnpmAndInstall(), parallel(bundle, packageModules));
-export const buildCi = series(tasks.cleanWithGit, tasks.prodSelectPnpmAndInstall(), parallel(bundle, packageModules));
+export const clean = tools.series(tasks.cleanWithGit());
+export const build = tools.series(tasks.selectPnpmAndInstall(), tools.parallel(bundle, packageModules));
+export const buildCi = tools.series(tasks.cleanWithGit(), tasks.prodSelectPnpmAndInstall(), tools.parallel(bundle, packageModules));
