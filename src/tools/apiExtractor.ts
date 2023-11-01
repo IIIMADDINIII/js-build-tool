be1f0114ab9d18d9896bef2a1f61e26e097bb55a
IIIMADDINIII <martin@elend.name>
2023-11-01 01:38:47 +0100

    Default Loglevel Warning

--- src/tools/apiExtractor.ts
+++ src/tools/apiExtractor.ts
@@ -1,16 +1,20 @@
-import { Extractor, ExtractorConfig, IConfigFile } from "@microsoft/api-extractor";
+import { Extractor, ExtractorConfig, ExtractorLogLevel, IConfigFile } from "@microsoft/api-extractor";
 import { resolve } from "path";
 import { resolveModule } from "./misc.js";
 
-export function runApiExtrator(projectPackageJsonPath: string, configObject: IConfigFile): void {
+export function runApiExtrator(projectPackageJsonPath: string, options: IConfigFile): void {
+  const configObject: IConfigFile = {
+    messages: {
+      compilerMessageReporting: { default: { logLevel: ExtractorLogLevel.Warning } },
+      extractorMessageReporting: { default: { logLevel: ExtractorLogLevel.Warning } },
+      tsdocMessageReporting: { default: { logLevel: ExtractorLogLevel.Warning } },
+    },
+    ...options
+  };
   const extractorConfig = ExtractorConfig.prepare({ configObject, configObjectFullPath: projectPackageJsonPath, packageJsonFullPath: projectPackageJsonPath });
   const extractorResult = Extractor.invoke(extractorConfig, {
     typescriptCompilerFolder: resolve(resolveModule("typescript"), "../.."),
   });
-  if (!extractorResult.succeeded) {
-
-    throw new Error(`API Extractor completed with ${extractorResult.errorCount} errors and ${extractorResult.warningCount} warnings`);
-  }
   if (extractorResult.succeeded) return console.log('API Extractor completed successfully');
   if (extractorResult.errorCount == 0) return console.warn('API Extractor completed with warnings');
   console.error('API Extractor completed with errors');
