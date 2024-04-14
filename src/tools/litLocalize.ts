d4d239175ba9087b16436839dd1254b0cf75b72f
IIIMADDINIII <martin@elend.name>
2024-04-14 20:45:38 +0200

    fix cache cant be const

--- src/tools/litLocalize.ts
+++ src/tools/litLocalize.ts
@@ -195,7 +195,7 @@ export async function transformTranslationFilesToUseDependencyInjection(translat
       // Replace all import statements with an empty string
       content = content.replaceAll(/^[^\S\r\n]*import[^\r\n]*$/mg, "");
       // Find export and surround it with a function 
-      content = content.replace(/^[^\S\r\n]*export([\s\S]*)/mg, "const cache = undefined;\nexport function templates(str, html) {\n  if (cache !== undefined) return cache;\n $1cache = {templates};\n  return cache;\n}");
+      content = content.replace(/^[^\S\r\n]*export([\s\S]*)/mg, "let cache = undefined;\nexport function templates(str, html) {\n  if (cache !== undefined) return cache;\n $1cache = {templates};\n  return cache;\n}");
       await fs.writeFile(file, content);
     }));
 }
