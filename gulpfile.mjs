b80fc8507c870f867df0f126b2a9b81b407375c5
IIIMADDINIII <martin@elend.name>
2024-04-18 18:59:28 +0200

    Automatically create a commit and Tag that commit on version

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -20,7 +20,11 @@ export async function version() {
   await tools.exec()`pnpm version -f --no-git-tag-version ${arg}`;
   const version = await tools.getPackageVersion(undefined, false);
   if (version === undefined) throw new Error("Could not Read main package Version.");
-  await tools.exec({ cwd: tools.file("./js-build-tool") })`pnpm version -f --no-git-tag-version ${version}`;
-  await tools.exec({ cwd: tools.file("./js-build-tool-cli") })`pnpm version -f --no-git-tag-version ${version}`;
-  await tools.exec({ cwd: tools.file("./js-build-tool-types") })`pnpm version -f --no-git-tag-version ${version}`;
+  await Promise.all([
+    tools.exec({ cwd: tools.file("./js-build-tool") })`pnpm version -f --no-git-tag-version ${version}`,
+    tools.exec({ cwd: tools.file("./js-build-tool-cli") })`pnpm version -f --no-git-tag-version ${version}`,
+    tools.exec({ cwd: tools.file("./js-build-tool-types") })`pnpm version -f --no-git-tag-version ${version}`,
+  ]);
+  await tools.createCommit({ message: `v${version}`, all: true });
+  await exec`git tag -m v${version} v${version}`;
 }
